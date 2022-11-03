26bb9cbe57ee8bd160a3970a1d6aed1849b4a515
Hemant Patil <Hemant Patil@Lenovo-PC>
2022-11-03 11:35:01 +0530

    Lab Session 4

---
+++ LabSession4/src/Shape.java
@@ -0,0 +1,22 @@
+public class Shape 
+{
+    
+    public static void main(String[] args)
+    {
+       CalculateArea ob = new CalculateArea();
+	   ob.area(4);
+	   ob.area(5.5);
+    }
+}
+class CalculateArea
+{
+    void area(float x)
+    {
+        System.out.println("The area of the square is "+Math.pow(x, 2)+" sq units");
+    }
+    void area(double x)
+    {
+        double z = 3.14 * x * x;
+        System.out.println("The area of the circle is "+z+" sq units");
+    }
+}
